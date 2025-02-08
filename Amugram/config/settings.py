da78b8f053110939a97f62ca44023780090ca2c7
shahab-bozorgi <shahab.bozorgi007@gmail.com>
2025-02-08 10:55:32 +0330

    change

--- Amugram/config/settings.py
+++ Amugram/config/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
-STATICFILES_DIRS = [BASE_DIR / "assets"]
+# STATICFILES_DIRS = [BASE_DIR / "assets"]
 
 
 MEDIA_URL = "/media/"
