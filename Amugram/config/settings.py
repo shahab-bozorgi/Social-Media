9fe7731510f0b023a0da28963162ce579311acc7
shahab-bozorgi <shahab.bozorgi007@gmail.com>
2025-03-06 23:39:48 +0330

    fix: dockerize

--- Amugram/config/settings.py
+++ Amugram/config/settings.py
@@ -94,10 +94,11 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'),
         'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
+
 
 
 
@@ -157,9 +158,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # STATICFILES_DIRS = [BASE_DIR / "assets"]
+
 
 
 MEDIA_URL = "/media/"
